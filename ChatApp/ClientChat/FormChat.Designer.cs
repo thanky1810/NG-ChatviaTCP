78a18424f5cda64591d4bc9458a0d283f738d242
Nam Nguyễn Thành <nguyenthanhnam100105@gmail.com>
2025-11-06 16:47:09 +0700

    Update formLogin and formChat

--- ChatApp/ClientChat/FormChat.Designer.cs
+++ ChatApp/ClientChat/FormChat.Designer.cs
@@ -40,7 +40,7 @@
             this.pnlFame = new System.Windows.Forms.Panel();
             this.rtbMessList = new System.Windows.Forms.RichTextBox();
             this.pnlHeader = new System.Windows.Forms.Panel();
-            this.lbNameRoom = new System.Windows.Forms.Label();
+            this.lblNameRoom = new System.Windows.Forms.Label();
             this.btnLeave = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
             this.txtMessInput = new System.Windows.Forms.TextBox();
@@ -58,9 +58,10 @@
             // 
             this.pnlSidebar.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pnlSidebar.Controls.Add(this.tcontlSidebar_);
-            this.pnlSidebar.Location = new System.Drawing.Point(12, 21);
+            this.pnlSidebar.Location = new System.Drawing.Point(11, 17);
+            this.pnlSidebar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlSidebar.Name = "pnlSidebar";
-            this.pnlSidebar.Size = new System.Drawing.Size(215, 499);
+            this.pnlSidebar.Size = new System.Drawing.Size(191, 399);
             this.pnlSidebar.TabIndex = 0;
             this.pnlSidebar.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -71,9 +72,10 @@
             this.tcontlSidebar_.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tcontlSidebar_.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tcontlSidebar_.Location = new System.Drawing.Point(0, 0);
+            this.tcontlSidebar_.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tcontlSidebar_.Name = "tcontlSidebar_";
             this.tcontlSidebar_.SelectedIndex = 0;
-            this.tcontlSidebar_.Size = new System.Drawing.Size(215, 499);
+            this.tcontlSidebar_.Size = new System.Drawing.Size(191, 399);
             this.tcontlSidebar_.TabIndex = 0;
             this.tcontlSidebar_.Tag = "";
             // 
@@ -81,20 +83,22 @@
             // 
             this.tpageUser.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.tpageUser.Controls.Add(this.lboxUsers);
-            this.tpageUser.Location = new System.Drawing.Point(4, 32);
+            this.tpageUser.Location = new System.Drawing.Point(4, 28);
+            this.tpageUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tpageUser.Name = "tpageUser";
-            this.tpageUser.Padding = new System.Windows.Forms.Padding(3);
-            this.tpageUser.Size = new System.Drawing.Size(207, 463);
+            this.tpageUser.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tpageUser.Size = new System.Drawing.Size(183, 367);
             this.tpageUser.TabIndex = 0;
             this.tpageUser.Text = "Users";
             // 
             // lboxUsers
             // 
             this.lboxUsers.FormattingEnabled = true;
-            this.lboxUsers.ItemHeight = 23;
+            this.lboxUsers.ItemHeight = 19;
             this.lboxUsers.Location = new System.Drawing.Point(1, 0);
+            this.lboxUsers.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lboxUsers.Name = "lboxUsers";
-            this.lboxUsers.Size = new System.Drawing.Size(207, 464);
+            this.lboxUsers.Size = new System.Drawing.Size(184, 365);
             this.lboxUsers.TabIndex = 0;
             this.lboxUsers.SelectedIndexChanged += new System.EventHandler(this.lboxUsers_SelectedIndexChanged);
             // 
@@ -103,10 +107,11 @@
             this.tpRoom.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.tpRoom.Controls.Add(this.panel1);
             this.tpRoom.Controls.Add(this.lboxRooms);
-            this.tpRoom.Location = new System.Drawing.Point(4, 32);
+            this.tpRoom.Location = new System.Drawing.Point(4, 28);
+            this.tpRoom.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tpRoom.Name = "tpRoom";
-            this.tpRoom.Padding = new System.Windows.Forms.Padding(3);
-            this.tpRoom.Size = new System.Drawing.Size(207, 463);
+            this.tpRoom.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tpRoom.Size = new System.Drawing.Size(183, 367);
             this.tpRoom.TabIndex = 1;
             this.tpRoom.Text = "Rooms";
             // 
@@ -115,16 +120,18 @@
             this.panel1.Controls.Add(this.btnCreate);
             this.panel1.Controls.Add(this.btnJoin);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(3, 332);
+            this.panel1.Location = new System.Drawing.Point(3, 263);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(201, 128);
+            this.panel1.Size = new System.Drawing.Size(177, 102);
             this.panel1.TabIndex = 1;
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(50, 76);
+            this.btnCreate.Location = new System.Drawing.Point(44, 61);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(96, 41);
+            this.btnCreate.Size = new System.Drawing.Size(85, 33);
             this.btnCreate.TabIndex = 2;
             this.btnCreate.Text = "Create";
             this.btnCreate.UseVisualStyleBackColor = true;
@@ -132,9 +139,10 @@
             // 
             // btnJoin
             // 
-            this.btnJoin.Location = new System.Drawing.Point(50, 19);
+            this.btnJoin.Location = new System.Drawing.Point(44, 15);
+            this.btnJoin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnJoin.Name = "btnJoin";
-            this.btnJoin.Size = new System.Drawing.Size(96, 41);
+            this.btnJoin.Size = new System.Drawing.Size(85, 33);
             this.btnJoin.TabIndex = 1;
             this.btnJoin.Text = "Join";
             this.btnJoin.UseVisualStyleBackColor = true;
@@ -146,10 +154,11 @@
             this.lboxRooms.FormattingEnabled = true;
             this.lboxRooms.HorizontalScrollbar = true;
             this.lboxRooms.IntegralHeight = false;
-            this.lboxRooms.ItemHeight = 25;
-            this.lboxRooms.Location = new System.Drawing.Point(3, 3);
+            this.lboxRooms.ItemHeight = 20;
+            this.lboxRooms.Location = new System.Drawing.Point(3, 2);
+            this.lboxRooms.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lboxRooms.Name = "lboxRooms";
-            this.lboxRooms.Size = new System.Drawing.Size(201, 326);
+            this.lboxRooms.Size = new System.Drawing.Size(179, 262);
             this.lboxRooms.TabIndex = 0;
             this.lboxRooms.SelectedIndexChanged += new System.EventHandler(this.lboxRooms_SelectedIndexChanged);
             // 
@@ -160,9 +169,10 @@
             this.pnlFame.Controls.Add(this.pnlHeader);
             this.pnlFame.Controls.Add(this.btnSend);
             this.pnlFame.Controls.Add(this.txtMessInput);
-            this.pnlFame.Location = new System.Drawing.Point(247, 21);
+            this.pnlFame.Location = new System.Drawing.Point(220, 17);
+            this.pnlFame.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlFame.Name = "pnlFame";
-            this.pnlFame.Size = new System.Drawing.Size(758, 499);
+            this.pnlFame.Size = new System.Drawing.Size(674, 399);
             this.pnlFame.TabIndex = 1;
             this.pnlFame.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlChatFame_Paint);
             // 
@@ -171,11 +181,12 @@
             this.rtbMessList.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.rtbMessList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.rtbMessList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbMessList.Location = new System.Drawing.Point(20, 63);
+            this.rtbMessList.Location = new System.Drawing.Point(18, 50);
+            this.rtbMessList.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rtbMessList.Name = "rtbMessList";
             this.rtbMessList.ReadOnly = true;
             this.rtbMessList.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.rtbMessList.Size = new System.Drawing.Size(700, 344);
+            this.rtbMessList.Size = new System.Drawing.Size(623, 276);
             this.rtbMessList.TabIndex = 4;
             this.rtbMessList.Text = "";
             this.rtbMessList.TextChanged += new System.EventHandler(this.rtbMessList_TextChanged);
@@ -183,32 +194,34 @@
             // pnlHeader
             // 
             this.pnlHeader.BackColor = System.Drawing.SystemColors.HotTrack;
-            this.pnlHeader.Controls.Add(this.lbNameRoom);
+            this.pnlHeader.Controls.Add(this.lblNameRoom);
             this.pnlHeader.Controls.Add(this.btnLeave);
             this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pnlHeader.Name = "pnlHeader";
-            this.pnlHeader.Size = new System.Drawing.Size(758, 57);
+            this.pnlHeader.Size = new System.Drawing.Size(674, 46);
             this.pnlHeader.TabIndex = 3;
             this.pnlHeader.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlChatHeader_Paint);
             // 
-            // lbNameRoom
+            // lblNameRoom
             // 
-            this.lbNameRoom.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.lbNameRoom.Location = new System.Drawing.Point(16, 13);
-            this.lbNameRoom.Name = "lbNameRoom";
-            this.lbNameRoom.Size = new System.Drawing.Size(195, 30);
-            this.lbNameRoom.TabIndex = 1;
-            this.lbNameRoom.Text = "NameRoom";
-            this.lbNameRoom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblNameRoom.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.lblNameRoom.Location = new System.Drawing.Point(14, 10);
+            this.lblNameRoom.Name = "lblNameRoom";
+            this.lblNameRoom.Size = new System.Drawing.Size(173, 24);
+            this.lblNameRoom.TabIndex = 1;
+            this.lblNameRoom.Text = "NameRoom";
+            this.lblNameRoom.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnLeave
             // 
             this.btnLeave.BackColor = System.Drawing.Color.DimGray;
             this.btnLeave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnLeave.Location = new System.Drawing.Point(645, 9);
+            this.btnLeave.Location = new System.Drawing.Point(573, 7);
+            this.btnLeave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLeave.Name = "btnLeave";
-            this.btnLeave.Size = new System.Drawing.Size(75, 39);
+            this.btnLeave.Size = new System.Drawing.Size(67, 31);
             this.btnLeave.TabIndex = 0;
             this.btnLeave.Text = "Leave";
             this.btnLeave.UseVisualStyleBackColor = false;
@@ -219,9 +232,10 @@
             this.btnSend.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.btnSend.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.btnSend.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
-            this.btnSend.Location = new System.Drawing.Point(645, 424);
+            this.btnSend.Location = new System.Drawing.Point(573, 339);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 60);
+            this.btnSend.Size = new System.Drawing.Size(67, 48);
             this.btnSend.TabIndex = 1;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = false;
@@ -230,10 +244,11 @@
             // txtMessInput
             // 
             this.txtMessInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMessInput.Location = new System.Drawing.Point(20, 424);
+            this.txtMessInput.Location = new System.Drawing.Point(18, 339);
+            this.txtMessInput.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtMessInput.Multiline = true;
             this.txtMessInput.Name = "txtMessInput";
-            this.txtMessInput.Size = new System.Drawing.Size(594, 60);
+            this.txtMessInput.Size = new System.Drawing.Size(528, 49);
             this.txtMessInput.TabIndex = 0;
             this.txtMessInput.TextChanged += new System.EventHandler(this.txtMessageInput_TextChanged);
             // 
@@ -241,9 +256,10 @@
             // 
             this.btnLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogOut.ForeColor = System.Drawing.Color.Red;
-            this.btnLogOut.Location = new System.Drawing.Point(858, 528);
+            this.btnLogOut.Location = new System.Drawing.Point(763, 422);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(147, 44);
+            this.btnLogOut.Size = new System.Drawing.Size(131, 35);
             this.btnLogOut.TabIndex = 2;
             this.btnLogOut.Text = "LogOut";
             this.btnLogOut.UseVisualStyleBackColor = true;
@@ -251,14 +267,15 @@
             // 
             // Chat_TCP_Client
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1026, 584);
+            this.ClientSize = new System.Drawing.Size(912, 467);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.pnlFame);
             this.Controls.Add(this.pnlSidebar);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Chat_TCP_Client";
             this.Text = "Chat Client";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Chat_TCP_Client_FormClosing);
@@ -288,7 +305,7 @@
         private System.Windows.Forms.Panel pnlHeader;
         private System.Windows.Forms.Button btnLeave;
         private System.Windows.Forms.Button btnLogOut;
-        private System.Windows.Forms.Label lbNameRoom;
+        private System.Windows.Forms.Label lblNameRoom;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.Button btnJoin;
--- ChatApp/ClientChat/FormLogin.Designer.cs
+++ ChatApp/ClientChat/FormLogin.Designer.cs
@@ -37,12 +37,18 @@
             this.txtHost = new System.Windows.Forms.TextBox();
             this.lblHost = new System.Windows.Forms.Label();
             this.lblLogin = new System.Windows.Forms.Label();
+            this.lblHost1 = new System.Windows.Forms.Label();
+            this.lblPort1 = new System.Windows.Forms.Label();
+            this.lblUserName1 = new System.Windows.Forms.Label();
             this.panelLogin.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelLogin
             // 
             this.panelLogin.BackColor = System.Drawing.SystemColors.Control;
+            this.panelLogin.Controls.Add(this.lblUserName1);
+            this.panelLogin.Controls.Add(this.lblPort1);
+            this.panelLogin.Controls.Add(this.lblHost1);
             this.panelLogin.Controls.Add(this.btnConnect);
             this.panelLogin.Controls.Add(this.txtUserName);
             this.panelLogin.Controls.Add(this.lblUserName);
@@ -51,111 +57,132 @@
             this.panelLogin.Controls.Add(this.txtHost);
             this.panelLogin.Controls.Add(this.lblHost);
             this.panelLogin.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelLogin.Location = new System.Drawing.Point(0, 134);
-            this.panelLogin.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelLogin.Location = new System.Drawing.Point(0, 108);
             this.panelLogin.Name = "panelLogin";
-            this.panelLogin.Size = new System.Drawing.Size(900, 428);
+            this.panelLogin.Size = new System.Drawing.Size(800, 342);
             this.panelLogin.TabIndex = 0;
+            this.panelLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLogin_Paint);
             // 
             // btnConnect
             // 
             this.btnConnect.BackColor = System.Drawing.Color.SpringGreen;
             this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConnect.Location = new System.Drawing.Point(359, 284);
-            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnConnect.Location = new System.Drawing.Point(325, 219);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(230, 49);
+            this.btnConnect.Size = new System.Drawing.Size(204, 39);
             this.btnConnect.TabIndex = 3;
             this.btnConnect.Text = "CONNECT";
             this.btnConnect.UseVisualStyleBackColor = false;
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(429, 196);
-            this.txtUserName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(325, 148);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(307, 26);
+            this.txtUserName.Size = new System.Drawing.Size(273, 30);
             this.txtUserName.TabIndex = 2;
-            this.txtUserName.Text = "Hãy nhập tên của bạn";
             // 
             // lblUserName
             // 
             this.lblUserName.AutoSize = true;
             this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUserName.Location = new System.Drawing.Point(197, 192);
+            this.lblUserName.Location = new System.Drawing.Point(118, 155);
             this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(170, 29);
+            this.lblUserName.Size = new System.Drawing.Size(145, 25);
             this.lblUserName.TabIndex = 0;
             this.lblUserName.Text = "USER NAME:";
             this.lblUserName.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtPort
             // 
-            this.txtPort.Location = new System.Drawing.Point(429, 141);
-            this.txtPort.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(325, 104);
             this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(307, 26);
+            this.txtPort.Size = new System.Drawing.Size(273, 30);
             this.txtPort.TabIndex = 1;
-            this.txtPort.Text = "VD: 8080";
             // 
             // lblPort
             // 
             this.lblPort.AutoSize = true;
             this.lblPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPort.Location = new System.Drawing.Point(197, 138);
+            this.lblPort.Location = new System.Drawing.Point(118, 111);
             this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(92, 29);
+            this.lblPort.Size = new System.Drawing.Size(78, 25);
             this.lblPort.TabIndex = 0;
             this.lblPort.Text = "PORT:";
             this.lblPort.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtHost
             // 
-            this.txtHost.Location = new System.Drawing.Point(429, 90);
-            this.txtHost.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHost.Location = new System.Drawing.Point(325, 63);
             this.txtHost.Name = "txtHost";
-            this.txtHost.Size = new System.Drawing.Size(307, 26);
+            this.txtHost.Size = new System.Drawing.Size(273, 30);
             this.txtHost.TabIndex = 0;
-            this.txtHost.Text = "VD: chatgui.server.com";
+            this.txtHost.TextChanged += new System.EventHandler(this.txtHost_TextChanged);
             // 
             // lblHost
             // 
             this.lblHost.AutoSize = true;
             this.lblHost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHost.Location = new System.Drawing.Point(197, 86);
+            this.lblHost.Location = new System.Drawing.Point(118, 70);
             this.lblHost.Name = "lblHost";
-            this.lblHost.Size = new System.Drawing.Size(92, 29);
+            this.lblHost.Size = new System.Drawing.Size(80, 25);
             this.lblHost.TabIndex = 0;
             this.lblHost.Text = "HOST:";
             this.lblHost.Click += new System.EventHandler(this.label2_Click);
             // 
             // lblLogin
             // 
-            this.lblLogin.AutoSize = true;
             this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLogin.ForeColor = System.Drawing.SystemColors.Control;
-            this.lblLogin.Location = new System.Drawing.Point(292, 42);
+            this.lblLogin.Location = new System.Drawing.Point(0, -1);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(372, 46);
+            this.lblLogin.Size = new System.Drawing.Size(800, 106);
             this.lblLogin.TabIndex = 1;
             this.lblLogin.Text = "LOGIN - CHATGUI";
+            this.lblLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHost1
+            // 
+            this.lblHost1.AutoSize = true;
+            this.lblHost1.Location = new System.Drawing.Point(613, 70);
+            this.lblHost1.Name = "lblHost1";
+            this.lblHost1.Size = new System.Drawing.Size(97, 16);
+            this.lblHost1.TabIndex = 4;
+            this.lblHost1.Text = "VD: 192.168.3.2";
+            // 
+            // lblPort1
+            // 
+            this.lblPort1.AutoSize = true;
+            this.lblPort1.Location = new System.Drawing.Point(613, 111);
+            this.lblPort1.Name = "lblPort1";
+            this.lblPort1.Size = new System.Drawing.Size(60, 16);
+            this.lblPort1.TabIndex = 4;
+            this.lblPort1.Text = "VD: 8080";
+            // 
+            // lblUserName1
+            // 
+            this.lblUserName1.Location = new System.Drawing.Point(613, 148);
+            this.lblUserName1.Name = "lblUserName1";
+            this.lblUserName1.Size = new System.Drawing.Size(139, 38);
+            this.lblUserName1.TabIndex = 4;
+            this.lblUserName1.Text = "Hãy nhập tên của bạn (tối đa 15 ký tự)";
             // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Navy;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.lblLogin);
             this.Controls.Add(this.panelLogin);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormLogin";
             this.Text = "Login";
             this.Load += new System.EventHandler(this.FormLogin_Load);
             this.panelLogin.ResumeLayout(false);
             this.panelLogin.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -170,5 +197,8 @@
         private System.Windows.Forms.Label lblUserName;
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblUserName1;
+        private System.Windows.Forms.Label lblPort1;
+        private System.Windows.Forms.Label lblHost1;
     }
 }
